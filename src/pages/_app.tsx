4bfd85ac3520c4f75db8b5572b2111706ce75341
Artem Onufriichuk <a.onufriichuk@artk.ai>
2024-07-14 21:16:44 +0300

    favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { defaultTheme } from '../themes';
 import useBootstrap from '../hooks/useBootstrap';
 import Sprite from 'components/Sprite/Sprite';
 import Preloader from 'components/Preloader/Preloader';
+import Head from 'next/head';
+import Favicon from '/public/favicon.ico';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	useBootstrap();
@@ -13,6 +15,15 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
 	return (
 		<>
+			<Head>
+				<link rel='icon' href={Favicon.src} sizes='any' />
+				<title>NOXU Solutions | Custom Software Development Company</title>
+				<meta name='title' content='Custom Software Development Company | NOXU Solutions' />
+				<meta
+					name='description'
+					content='At NOXU Solutions, we specialize in full-cycle custom software development. From ideation to launch, our expert team provides tailored solutions in product development, UX/UI design, and branding to elevate your business. Partner with us to transform your ideas into exceptional digital products and ensure your success in the competitive tech landscape..'
+				/>
+			</Head>
 			<ThemeProvider theme={defaultTheme}>
 				<Sprite />
 				<GlobalStyle />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,21 @@
+import GoogleAnalytics from 'components/GoogleAnalytics';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
 	return (
 		<Html>
 			<Head>
-				<link rel='icon' href='/favicon.ico' />
 				<meta httpEquiv='X-UA-Compatible' content='ie=edge' />
 				<meta name='viewport' content='width=device-width , initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' />
-				<meta name='title' content='Custom Software Development Company | NOXU Solutions' />
-				<meta
-					name='description'
-					content='At NOXU Solutions, we specialize in full-cycle custom software development. From ideation to launch, our expert team provides tailored solutions in product development, UX/UI design, and branding to elevate your business. Partner with us to transform your ideas into exceptional digital products and ensure your success in the competitive tech landscape..'
-				/>
-
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
 				<link href='https://fonts.googleapis.com/css2?family=Golos+Text:wght@400..900&display=swap' rel='stylesheet' />
 			</Head>
 			<body>
 				<Main />
-				<NextScript />
+				<NextScript>
+					<GoogleAnalytics />
+				</NextScript>
 			</body>
 		</Html>
 	);
