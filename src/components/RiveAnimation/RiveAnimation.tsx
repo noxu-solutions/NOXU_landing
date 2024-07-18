ace4dfa37d8b9a8d7a911a7d6f2043a260cab72c
Artem Onufriichuk <a.onufriichuk@artk.ai>
2024-07-19 00:34:11 +0300

    animation start if in view

--- src/components/RiveAnimation/RiveAnimation.tsx
+++ src/components/RiveAnimation/RiveAnimation.tsx
@@ -1,5 +1,7 @@
 import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
 import { RiveAnimationContainer } from './RiveAnimation.styles';
+import { useEffect, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export interface IconProps {
 	src: string;
@@ -17,10 +19,13 @@ const RiveAnimation: React.FC<IconProps> = ({ src }) => {
 
 	const { RiveComponent } = useRive(params);
 
+	const { ref, inView } = useInView({
+		triggerOnce: true, // Запускать только один раз при входе в область видимости
+		threshold: 0.1, // Запускать, когда хотя бы 1% компонента видимо
+	});
+
 	return (
-		<RiveAnimationContainer>
-			<RiveComponent style={{ width: '100%', alignSelf: 'stretch', height: '100%' }} />
-		</RiveAnimationContainer>
+		<RiveAnimationContainer ref={ref}>{inView && <RiveComponent style={{ width: '100%', alignSelf: 'stretch', height: '100%' }} />}</RiveAnimationContainer>
 	);
 };
 
